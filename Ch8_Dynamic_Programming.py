8057f7ca90efcc56cbf1a714c02f0300c6f7adb8
joohan-lee <joohan0224@naver.com>
2021-03-03 00:12:43 +0900

    Update Ch8_Dynamic_Programming.py

--- Ch8_Dynamic_Programming.py
+++ Ch8_Dynamic_Programming.py
@@ -72,3 +72,33 @@ for i in range(3, n+1):
 print(d[n])
 
 
+#================================================================================
+#실전문제4. 효율적인 화폐 구성
+# N종류의 화폐를 이용하여 M원을 만들고자 할때,
+# 최소 화폐 개수는? (1<=N<=100, 1<=M<=10000)
+
+n, m = map(int, input().split())
+
+paper = []
+for i in range(n):
+  paper.append(int(input())) #N종류의 화폐 가치
+
+
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n):
+  for j in range(paper[i],m+1): #화폐 단위마다 만들 수 있는 개수 계산
+    if d[j - paper[i]] != 10001: #(i-k)원을 만드는 방법이 존재하는 경우
+      d[j] = min(d[j], d[j-paper[i]] + 1)
+    
+if d[m] == 10001:
+  print(-1)
+else:
+  print(d[m])
+
+
+
+
+
+
