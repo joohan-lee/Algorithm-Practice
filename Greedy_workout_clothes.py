c91afa32775f9875871003b5fe2431d52eac58ac
joohan-lee <joohan0224@naver.com>
2021-03-09 14:03:34 +0900

    Update Greedy_workout_clothes.py

--- Greedy_workout_clothes.py
+++ Greedy_workout_clothes.py
@@ -16,3 +16,21 @@ def solution(n, lost, reserve):
   
   
   ###################################################
+    
+#풀이2. - 210309
+#통과(100/100)
+
+def solution(n, lost, reserve):
+    answer = 0
+    set_lost = set(lost) - set(reserve) #여벌있고 도난당한 사람 제거
+    set_reserve = set(reserve) - set(lost) #여벌있고 도난당한 사람 제거
+    
+    for person_num in set_reserve: #빌려줄 수 있는 사람 모두 검사
+        if person_num - 1 in set_lost: #빌려줄 수 있는 사람 번호보다 1 작으면 빌려줌
+            set_lost.remove(person_num-1)
+        elif person_num + 1 in set_lost: #빌려줄 수 있는 사람 번호보다 1 크면 빌려줌
+            set_lost.remove(person_num + 1)
+    
+    answer = n - len(set_lost)
+    
+    return answer
